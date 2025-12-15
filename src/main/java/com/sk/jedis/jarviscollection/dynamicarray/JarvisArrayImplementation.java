69ed7a47b61807042478618cbae1f6d3a4e5beb5
devHub24 <devhub2024@gmail.com>
2025-12-15 22:48:20 +0530

    feat: Add append feature

---
+++ src/main/java/com/sk/jedis/jarviscollection/dynamicarray/JarvisArrayImplementation.java
@@ -0,0 +1,74 @@
+package com.sk.jedis.jarviscollection.dynamicarray;
+
+public class JarvisArrayImplementation<T> implements JarvisArray<T> {
+
+    private Object[] elements;
+    private int size;
+
+    public JarvisArrayImplementation(){
+        this.size =10;
+        this.elements = new Object[10];
+    }
+
+    public JarvisArrayImplementation(int initialCapacity){
+        this.size =initialCapacity;
+        this.elements = new Object[initialCapacity];
+    }
+
+    @Override
+    public boolean add(T element) {
+       try {
+           if (this.size >= this.elements.length) {
+               resize();
+           }
+           elements[this.size++] = element;
+           grow();
+       }catch(Exception ex){
+           return false;
+       }
+        return true;
+    }
+
+    @Override
+    public boolean add(int index, T element) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean set(int index, T element) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elements.length ==0;
+    }
+
+    private void resize(){
+        int newCapacity = this.size*2;
+        Object[] newArray = new Object[newCapacity];
+        for(int i=0;i<newCapacity;i++){
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+    }
+
+    private int grow(){
+        return this.size*2;
+    }
+}
